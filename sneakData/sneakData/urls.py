b4dc868cefd61253e153c0964b4df1367c578c92
jinzhou77 <jinzhou3@illinois.edu>
2021-07-03 03:50:24 -0500

    test

--- sneakData/sneakData/urls.py
+++ sneakData/sneakData/urls.py
@@ -3,6 +3,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('api/', include('sneakers.urls')),
-    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
