1ee7e8aea7958c0df0a4c18af90e1c1c6352a62c
jinzhou77 <jinzhou3@illinois.edu>
2021-07-15 23:40:42 -0500

    fixed typo

--- sneakData/sneakers/models.py
+++ sneakData/sneakers/models.py
@@ -11,7 +11,7 @@ class Detail(models.Model):
     image_path = models.CharField(max_length=255, null=True)
     retail_price = models.DecimalField(null=True, default=0.0, decimal_places=2, max_digits=10)
     number_of_sale = models.IntegerField(default=0)
-    averge_sale_price = models.DecimalField(null=True, default=0.0, decimal_places=2, max_digits=10)
+    average_sale_price = models.DecimalField(null=True, default=0.0, decimal_places=2, max_digits=10)
     style_code = models.CharField(max_length=255, null=True)
     colorway = models.CharField(max_length=255, null=True)
     ticker = models.CharField(max_length=255, null=True)
